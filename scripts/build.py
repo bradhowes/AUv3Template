e0d7cf7d2da68345af4cf2f85823dbe95b9bf3c3
Brad Howes <bradhowes@mac.com>
2023-10-20 20:07:35 +0100

    Cleanup script

--- scripts/build.py
+++ scripts/build.py
@@ -9,7 +9,6 @@
 
 import os
 import os.path
-import re
 import shutil
 import subprocess
 import sys
@@ -18,16 +17,21 @@ import sys
 def substituteName(content, name, subtype):
     return content.replace('--NAME--', name).replace('--SUBTYPE--', subtype)
 
+
 def makeDestPath(sourcePath, destDir, name, subtype):
     return substituteName(os.path.join(destDir, sourcePath), name, subtype)
 
+
 def buildFile(sourcePath, destPath, name, subtype):
-    justCopy = ['.py', '.png', '.caf', '.wav', '.xcuserstate', '.opacity', '.ttf']
+    justCopy = ['.py', '.png', '.caf', '.wav', '.xcuserstate', '.opacity',
+                '.ttf']
     os.makedirs(os.path.split(destPath)[0], exist_ok=True)
     if os.path.splitext(destPath)[1] in justCopy:
         shutil.copy2(sourcePath, destPath)
     else:
-        open(destPath, 'w').write(substituteName(open(sourcePath, 'r').read(), name, subtype))
+        open(destPath, 'w').write(substituteName(open(sourcePath, 'r').read(),
+                                                 name, subtype))
+
 
 def runCommand(step, *args):
     print('--', step)
@@ -37,11 +41,14 @@ def runCommand(step, *args):
         print(process.stderr)
         sys.exit(1)
 
+
 def createRepo(destDir):
     os.chdir(destDir)
     runCommand('creating repo', 'git', 'init')
     runCommand('stagiing files', 'git', 'add', '-A')
-    runCommand('committing to repo', 'git', 'commit', '-m', 'Yeah! Initial commit')
+    runCommand('committing to repo', 'git', 'commit', '-m',
+               'Yeah! Initial commit')
+
 
 def build(name, subtype):
     destDir = os.path.join('..', name)
@@ -53,12 +60,18 @@ def build(name, subtype):
             if ignore in dirnames:
                 dirnames.remove(ignore)
         for filename in filenames:
-            if filename == '.DS_Store': continue
+            if filename == '.DS_Store':
+                continue
             sourcePath = os.path.join(dirname, filename)[2:]
             print('--', sourcePath)
-            buildFile(sourcePath, makeDestPath(sourcePath, destDir, name, subtype), name, subtype)
+            buildFile(sourcePath,
+                      makeDestPath(sourcePath, destDir, name, subtype),
+                      name,
+                      subtype)
     createRepo(destDir)
-    runCommand('opening project', 'open', os.path.join(destDir, name + '.xcodeproj'))
+    runCommand('opening project', 'open',
+               os.path.join(destDir, name + '.xcodeproj'))
+
 
 if __name__ == '__main__':
     build(sys.argv[1], sys.argv[2])

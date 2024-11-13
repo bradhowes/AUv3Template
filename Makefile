78ccd5df2f1f3a2c52f80beae30e184228b61ed6
Brad Howes <bradhowes@mac.com>
2024-11-13 21:56:45 +0100

    Another attempt to fix CI.

--- Makefile
+++ Makefile
@@ -1,29 +1,26 @@
 PLATFORM_IOS = iOS Simulator,name=iPad mini (A17 Pro)
 PLATFORM_MACOS = macOS
+XCCOV = xcrun xccov view --report --only-targets
 
-default: build
-
-build-iOS:
-	rm -rf "$(PWD)/.DerivedData-iOS"
-	xcodebuild build \
-		-scheme 'iOS App' \
-		-derivedDataPath "$(PWD)/.DerivedData-iOS" \
-		-destination platform="$(PLATFORM_IOS)"
+default: report
 
 test-iOS:
 	rm -rf "$(PWD)/.DerivedData-iOS"
 	xcodebuild test \
 		-scheme 'iOS App' \
 		-derivedDataPath "$(PWD)/.DerivedData-iOS" \
-		-destination platform="$(PLATFORM_IOS)"
+		-destination platform="$(PLATFORM_IOS)" \
+		-enableCodeCoverage YES
 
-build-macOS:
-	rm -rf "$(PWD)/.DerivedData-macOS"
-	xcodebuild build \
-		-scheme 'macOS App' \
-		-derivedDataPath "$(PWD)/.DerivedData-macOS" \
-		-destination platform="$(PLATFORM_MACOS)" \
-	    CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
+coverage-iOS: test-iOS
+	$(XCCOV) "$(PWD)/.DerivedData-iOS/Logs/Test/Test-iOS App-"*.xcresult > coverage_iOS.txt
+	echo "iOS Coverage:"
+	cat coverage_iOS.txt
+
+percentage-iOS: coverage-iOS
+	awk '/ AUv3-Support / { print $$4 }' coverage_iOS.txt > percentage_iOS.txt
+	echo "iOS Coverage Pct:"
+	cat percentage_iOS.txt
 
 test-macOS:
 	rm -rf "$(PWD)/.DerivedData-macOS"
@@ -31,11 +28,25 @@ test-macOS:
 		-scheme 'macOS App' \
 		-derivedDataPath "$(PWD)/.DerivedData-macOS" \
 		-destination platform="$(PLATFORM_MACOS)" \
+		-enableCodeCoverage YES \
 	    CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
 
-build: test-iOS test-macOS
+coverage-macOS: test-macOS
+	$(XCCOV) "$(PWD)/.DerivedData-macOS/Logs/Test/Test-macOS App-"*.xcresult > coverage_macOS.txt
+	echo "macOS Coverage:"
+	cat coverage_macOS.txt
 
-.PHONY: build build-iOS test-macOS
+percentage-macOS: coverage-macOS
+	awk '/ AUv3-Support / { print $$4 }' coverage_macOS.txt > percentage_macOS.txt
+	echo "macOS Coverage Pct:"
+	cat percentage_macOS.txt
+
+report: percentage-iOS percentage-macOS
+	@if [[ -n "$$GITHUB_ENV" ]]; then \
+        echo "PERCENTAGE=$$(< percentage_macOS.txt)" >> $$GITHUB_ENV; \
+    fi
+
+.PHONY: report test-iOS test-macOS coverage-iOS coverage-macOS coverage-iOS percentage-macOS percentage-iOS
 
 clean:
-	-rm -rf $(PWD)/.DerivedData-macOS $(PWD)/.DerivedData-iOS
+	-rm -rf $(PWD)/.DerivedData-macOS $(PWD)/.DerivedData-iOS coverage*.txt percentage*.txt
--- Packages/Tests/KernelTests/KernelTests.mm
+++ Packages/Tests/KernelTests/KernelTests.mm
@@ -24,31 +24,31 @@
   AVAudioFormat* format = [[AVAudioFormat alloc] initStandardFormatWithSampleRate:44100.0 channels:2];
   kernel->setRenderingFormat(1, format, 100, 20.0);
 
-  kernel->setParameterValuePending(ParameterAddressDepth, 10.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressDepth), 10.0, 0.001);
+  kernel->setParameterValue(ParameterAddressDepth, 10.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressDepth), 10.0, 0.001);
 
-  kernel->setParameterValuePending(ParameterAddressDelay, 20.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressDelay), 20.0, 0.001);
+  kernel->setParameterValue(ParameterAddressDelay, 20.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressDelay), 20.0, 0.001);
 
-  kernel->setParameterValuePending(ParameterAddressRate, 30.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressRate), 30.0, 0.001);
+  kernel->setParameterValue(ParameterAddressRate, 30.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressRate), 30.0, 0.001);
 
-  kernel->setParameterValuePending(ParameterAddressFeedback, 40.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressFeedback), 40.0, 0.001);
+  kernel->setParameterValue(ParameterAddressFeedback, 40.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressFeedback), 40.0, 0.001);
 
-  kernel->setParameterValuePending(ParameterAddressDry, 50.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressDry), 50.0, 0.001);
+  kernel->setParameterValue(ParameterAddressDry, 50.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressDry), 50.0, 0.001);
 
-  kernel->setParameterValuePending(ParameterAddressWet, 60.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressWet), 60.0, 0.001);
+  kernel->setParameterValue(ParameterAddressWet, 60.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressWet), 60.0, 0.001);
 
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressNegativeFeedback), 0.0, 0.001);
-  kernel->setParameterValuePending(ParameterAddressNegativeFeedback, 1.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressNegativeFeedback), 1.0, 0.001);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressNegativeFeedback), 0.0, 0.001);
+  kernel->setParameterValue(ParameterAddressNegativeFeedback, 1.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressNegativeFeedback), 1.0, 0.001);
 
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressOdd90), 0.0, 0.001);
-  kernel->setParameterValuePending(ParameterAddressOdd90, 1.0);
-  XCTAssertEqualWithAccuracy(kernel->getParameterValuePending(ParameterAddressOdd90), 1.0, 0.001);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressOdd90), 0.0, 0.001);
+  kernel->setParameterValue(ParameterAddressOdd90, 1.0);
+  XCTAssertEqualWithAccuracy(kernel->getParameterValue(ParameterAddressOdd90), 1.0, 0.001);
 }
 
 @end

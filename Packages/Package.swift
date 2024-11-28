990b12f0af817a8368dc6c92143c649edab28b7d
Brad Howes <bradhowes@mac.com>
2024-11-28 21:37:30 +0100

    Use lsatest AUv3Support

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     .library(name: "Theme", targets: ["Theme"])
   ],
   dependencies: [
-    .package(url: "https://github.com/bradhowes/AUv3Support", exact: "15.0.1"),
+    .package(url: "https://github.com/bradhowes/AUv3Support", exact: "16.0.2"),
   ],
   targets: [
     .target(
